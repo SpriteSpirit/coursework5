b4c8b67ab73a5be9779cbbfb4464de0e6b1b3a04
Sprite_Spirit <74331153+SpriteSpirit@users.noreply.github.com>
2024-03-02 00:34:05 +0300

    - реализован методы для вывода на печать

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from src.data.db_create import DBCreate
 from src.storage.json_saver import JSONSaver
-from user_interaction import print_all_companies_and_vacancies
+from user_interaction import *
 
 
 def main():
@@ -23,13 +23,23 @@ def main():
     db_manager = DBCreate.create_db_and_insert_tables(db_name, selected_employers)
 
     # ---------- companies_and_vacancies ----------#
-    print("Компании и количество вакансий:")
     companies_and_vacancies = db_manager.get_companies_and_vacancies_count()
     print_all_companies_and_vacancies(companies_and_vacancies)
+    JSONSaver.save_into_json_file(companies_and_vacancies, 'companies_and_vacancies')
 
     # ---------- all_vacancies ----------#
-    print(f"\nВсе вакансии:")
     all_vacancies = db_manager.get_all_vacancies()
+    print_all_vacancies(all_vacancies)
+    JSONSaver.save_into_json_file(all_vacancies, 'all_vacancies')
+
+    # ---------- avg_salary ----------#
+    avg_salary = db_manager.get_avg_salary()
+    print_avg_salary(avg_salary)
+
+    # ---------- vacancies_higher_avg_salary ----------#
+    vacancies_higher_avg_salary = db_manager.get_vacancies_with_higher_salary()
+    print_vacancies_higher_avg_salary(vacancies_higher_avg_salary)
+    JSONSaver.save_into_json_file(vacancies_higher_avg_salary, 'vacancies_higher_avg_salary')
 
 
 if __name__ == '__main__':
--- src/user_interaction.py
+++ src/user_interaction.py
@@ -1,9 +1,63 @@
 
 
 def print_all_companies_and_vacancies(total_list: list) -> None:
+    """  """
+
+    print("Компании и количество вакансий:")
+
     for items in total_list:
         print(items)
 
     all_vacancies = sum([vac[1] for vac in total_list])
 
     print(f"\nКомпаний: {len(total_list)}\nВакансий: {all_vacancies}")
+
+
+def print_all_vacancies(total_list: list) -> None:
+    """  """
+
+    print_dict = {}
+
+    for vacancy in total_list:
+        company_name = vacancy[0]
+        vacancy_details = f'{vacancy[1]}, {vacancy[2]}, {vacancy[3]}'
+
+        if company_name in print_dict:
+            print_dict[company_name].append(vacancy_details)
+        else:
+            print_dict[company_name] = [vacancy_details]
+
+    print(f"\nВсе вакансии:")
+
+    # Выводим вакансии для каждой компании
+    for company, vacancies in print_dict.items():
+        print(f'\n{company}:')
+        for i, vacancy_details in enumerate(vacancies, 1):
+            print(f'{i}. {vacancy_details}')
+
+
+def print_avg_salary(avg_salary: float) -> None:
+    """  """
+    print(f"Средняя зарплата по вакансиям: {avg_salary} RUB.")
+
+
+def print_vacancies_higher_avg_salary(total_list: list) -> None:
+    print_dict = {}
+
+    for vacancy in total_list:
+        company_name = vacancy[3]
+        vacancy_details = {'Название вакансии': vacancy[1], 'ID вакансии': vacancy[0], 'Город': vacancy[4],
+                           'Зарплата': vacancy[5], 'Ссылка': vacancy[6], 'ID компании': vacancy[2]}
+
+        if company_name in print_dict:
+            print_dict[company_name].append(vacancy_details)
+        else:
+            print_dict[company_name] = [vacancy_details]
+
+    print(f"\nВсе вакансии:")
+
+    # Выводим вакансии для каждой компании
+    for company, vacancies in print_dict.items():
+        print(f'\n{company}:')
+        for i, vacancy_details in enumerate(vacancies, 1):
+            print(f'{i}. {vacancy_details}')
